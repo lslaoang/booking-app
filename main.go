5da07ba57017934a7f9cb2876123d5221c5e453b
Laurel Lao Ang <laurellaoang@gmail.com>
2022-12-08 18:04:24 +0800

    print format

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets= 50
+	fmt.Printf("Welcome to %s booking application! \n", conferenceName)
+	fmt.Printf("We  have total of %d tickets and %d are still available. \n", conferenceTickets, remainingTickets	)
+	fmt.Println("Get your tickets here to attend.")
 }
