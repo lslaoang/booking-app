4d82d0d29ac808ecaca8dcbe9f6b56e40d81f9b2
Laurel Lao Ang <laurellaoang@gmail.com>
2022-12-09 16:32:59 +0800

    function call

--- main.go
+++ main.go
@@ -5,11 +5,7 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets= 50
-	fmt.Printf("Welcome to %s booking application! \n", conferenceName)
-	fmt.Printf("We  have total of %d tickets and %d are still available. \n", conferenceTickets, remainingTickets	)
-	fmt.Println("Get your tickets here to attend.")
-
+	var remainingTickets = 50
 	var userName string
 	var userTickets int
 	//ask user to enter name
@@ -17,8 +13,18 @@ func main() {
 	userName = "Integer"
 	userTickets = 3
 
-	fmt.Printf("User %v booked %v tickets. \n",userName , userTickets)
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
 	fmt.Printf("conferenceTickets is %T	conferenceName is %T remainingTickets is %T	userName is %T 	userTickets is %T",
-	conferenceTickets, conferenceName, remainingTickets, userName, userTickets)
+		conferenceTickets, conferenceName, remainingTickets, userName, userTickets)
 
+	printThisLine()
+}
+
+func printThisLine() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to %s booking application! \n", conferenceName)
+	fmt.Printf("We  have total of %d tickets and %d are still available. \n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
 }
